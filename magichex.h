48017a4e0a01a2cba1dc78148662638d729391cd
Jonas Bodingbauer <jonas.bodingbauer@tuwien.ac.at>
2023-12-12 21:22:23 +0100

    Added Alldiff again

---
+++ magichex.h
@@ -0,0 +1,16 @@
+#ifndef __MAGICHEX_H__
+#define __MAGICHEX_H__
+
+/* constraint variable; if lo==hi, this is the variable's value */
+typedef struct var {
+  long id; /* variable id; id<0 if the variable is not part of the hexagon */
+  long lo; /* lower bound */
+  long hi; /* upper bound */
+  unsigned long lorank;
+  unsigned long hirank;
+} Var;
+
+int sethi(Var *v, long x);
+int setlo(Var *v, long x);
+
+#endif
